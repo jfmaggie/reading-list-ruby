746c8302cdb4d34e046b4878382aa5cd7d1a1378
Meichen Zhou <jfmaggie@gmail.com>
2016-06-07 13:41:40 -0700

    Completed dynamically adding pagination links

--- app/assets/javascripts/shared.js
+++ app/assets/javascripts/shared.js
@@ -1,6 +1,8 @@
 function addNewPage(resource) {
   var nextPageNumber = parseInt($(".page.current").text()) + 1;
   $(".page.current").after("<span class='page'><a rel='next' href='" + resource + "?page="+ nextPageNumber + "'>" + nextPageNumber + "</a></span>");
+  $(".pagination").append("<span class='page'><a rel='next' href='" + resource + "?page="+ nextPageNumber + "'>Next › </a></span>");
+  $(".pagination").append("<span class='last'><a rel='next' href='" + resource + "?page="+ nextPageNumber + "'>Last » </a></span>");
 }
 
 function isLastPage() {
