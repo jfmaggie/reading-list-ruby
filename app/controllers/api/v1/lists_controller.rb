b87db55784dcd7d2b7ba038a37cb62941c433d27
Meichen Zhou <jfmaggie@gmail.com>
2016-06-17 13:13:42 -0700

    Finish the Api::V1::ListsController: - added list update endpoint - added list destroy endpoint

--- app/controllers/api/v1/lists_controller.rb
+++ app/controllers/api/v1/lists_controller.rb
@@ -1,15 +1,17 @@
 class Api::V1::ListsController < ApiController
-  before_action :find_list, only: [:show]
-  # GET api/v1/lists
+  before_action :find_list, only: [:show, :update, :destroy]
+  # GET /api/v1/lists
   def index
     @lists = List.all
     render json: @lists
   end
 
+  # GET /api/v1/lists/:id
   def show
     render json: @list
   end
 
+  # POST /api/v1/lists
   def create
     @list = List.new(list_params)
 
@@ -18,6 +20,22 @@ class Api::V1::ListsController < ApiController
     else
       render json: @list.errors, status: :unprocessable_entity
     end
+  end
+
+  # PUT /api/v1/lists/:id
+  def update
+    if @list.update(list_params)
+      # if the update succeeds
+      render json: @list
+    else
+      render json: @list.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /api/v1/lists/:id
+  def destroy
+    @list.destroy
+    render json: { success: true }
   end
 
   private
