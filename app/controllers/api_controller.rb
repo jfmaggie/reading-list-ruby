06d24e90e86f52e129a7a8679c5b1493f1bc04aa
Meichen Zhou <jfmaggie@gmail.com>
2016-06-21 23:54:38 -0700

    Set headers to allow CORS

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,3 +1,12 @@
 class ApiController < ActionController::Base
   protect_from_forgery with: :null_session
+  before_action :set_headers
+
+  def set_headers
+    headers['Access-Control-Allow-Origin'] = 'http://localhost:8080'
+    headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
+    headers['Access-Control-Request-Method'] = '*'
+    headers['Access-Control-Allow-Headers'] = 'Origin, X-Request-With, Content-Type, Accept, Authorization'
+  end
+
 end
